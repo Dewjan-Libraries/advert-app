24bec8cd45268addc6e3131fe374f35dc85e6600
Deborah Amuzu <debamuzudeb@gmail.com>
2024-10-16 15:54:07 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,25 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import React from 'react'
 
 import './App.css'
+import VendorDashBoard from './pages/VendorDashBoard'
+import MyAdPage from './pages/myAdsPage'
+import PostAd from './pages/postAdsForm'
 
 function App() {
   const   router = createBrowserRouter ([
 
-
+{
+  path:'/vendor-dashboard',
+  element:<VendorDashBoard/>
+},
+{
+  path:'/myads',
+  element:<MyAdPage/>
+},
+{
+  path:'/post-ad',
+  element:<PostAd/>
+}
 
 
     
---
+++ src/pages/vendordashboard/sidebarboard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SidebarDashBoard = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default SidebarDashBoard;
+
+{
+  /* <div>
+        <img src="" alt="" />
+        <h1>Dashboard</h1>
+        <h1>My Ads</h1>
+      </div> */
+}
